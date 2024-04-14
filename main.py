63d2feb40310d9f6e10ec03848a7fa0b3c237993
Jusai16 <144591978+Jusai16@users.noreply.github.com>
2024-04-14 14:27:38 +0800

    Update main.py
    Добавлена функция отправки картинок

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+# импорт библиотек
 import discord 
 from discord.ext import commands
 from keras.models import load_model  # TensorFlow is required for Keras to work
 from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
-
+# создание бота
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-
+# подключение модели
 def get_class(image, model, classes):
   np.set_printoptions(suppress=True)
   
@@ -40,19 +41,27 @@ def get_class(image, model, classes):
   
   return index
 
+
+# сообщение о готовности
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f'Бот {bot.user} готов к использованию')
 
+
+# приветствие
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f'Hi! I am a bot {bot.user}!')
+    await ctx.send(f'Привет! Я бот - {bot.user} проекта GreenGrow. И я помогу тебе с выбором саженцов для твоего участка!')
+    await ctx.send("Перед началом работы советую прочитать раздел $info")
+    await ctx.send("Как начать работу программы? Введите сообщение $soil, затем выберете картинку и отправьте сообщение.")
+
 
 @bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
+async def info(ctx):
+    await ctx.send("В 1771 году английский священник и естествоиспытатель Джозеф Пристли решил проверить правда ли, что выдыхаемый воздух становится «грязным» и может негативно воздействовать на живые организмы. Для этого он посадил под герметичный колпак маленького мышонка и начал свой простой, но очень информативный опыт. Через некоторое время мышь ослабла, а потом и вовсе умерла в страшных мучениях. Затем Пристли решил добавить в эксперимент растение и поместил под колпак не только мышь, но и росточек мяты. Мышь совершенно свободно перемещалась под колпаком и довольно долгое время чувствовала себя отлично. Так Джозеф Пристли первым изучил влияние растений на состав окружающего воздуха.")
 
 
+# загрузка фото
 @bot.command()
 async def test(ctx):
     files = ctx.message.attachments 
@@ -62,9 +71,9 @@ async def test(ctx):
     syf = files[0].filename.split(".")[-1]
     await files[0].save(f"file.{syf}" )
 
-
+# защита + ответ от модели 
 @bot.command()
-async def predict(ctx):
+async def soil(ctx):
     files = ctx.message.attachments 
     syf = files[0].filename.split(".")[-1]
     
@@ -79,13 +88,65 @@ async def predict(ctx):
     
     if indx == 0:
         await ctx.send("Глинистые почвы")
-        
+        await ctx.send("На глинистых почвах хорошо растут и даже дают урожай: яблоня, вишня, рябина.")
+        await ctx.send(" Данные культуры не являются рекордсменами по выработки кислорода, но принесут приятную пользу для вас и вашего сада.")
+        with open('images/яблоня.jpg', 'rb') as f:
+             picture = discord.File(f)
+        await ctx.send(file=picture)
+        with open('images/вишня.jpg', 'rb') as f:
+             picture = discord.File(f)
+        await ctx.send(file=picture)
+        with open('images/рябина.jpg', 'rb') as f:
+             picture = discord.File(f)
+        await ctx.send(file=picture)
+        await ctx.send("Рекомендации по уходу: саженцы сильно подвержены вредителям, поэтому они требуют большого внимания и качественного ухода. Кроме полива и подкормки эти растения нужно обрезать весной и тщательно следить за сорниками вокруг стволов дерева.")
+
+      
     if indx == 1:
          await ctx.send("Супеси")
+         await ctx.send("Для супесных почв подходят хвойные растения: ели, можжевельник, лиственница.")
+         await ctx.send("Хвойные растения черезвычайно полезны из-за выработки фитонцидов, которые оказывают антибактериальные свойства и очищают воздух от болезнетворных бактерий, а также эти растения удерживают в своих кронах от 30-50 тонн пыли.")
+         with open('images/ель.jpg', 'rb') as f:
+             picture = discord.File(f)
+         await ctx.send(file=picture)
+         with open('images/можжевельник.jpg', 'rb') as f:
+             picture = discord.File(f)
+         await ctx.send(file=picture)
+         with open('images/лиственница.jpg', 'rb') as f:
+             picture = discord.File(f)
+         await ctx.send(file=picture)
+         await ctx.send("Рекомендации по уходу: Молодые растения желательно поливать 1 раз в неделю, особенно в жаркий период. Взрослые растения поливаются 2-3 раза за сезон, но достаточно большим количеством воды. Без исключения все хвойные любят частое дождевание (опрыскивание из шланга кроны дерева), которое следует производить в утренние или вечерние часы.")
+      
     if indx == 2:
          await ctx.send("Песчанная")
-    if indx == 1:
+         await ctx.send("Дуб, Каштан и Тополь не любят заболоченные почвы, поэтому им хорошо подхлдят песчанные почвы.")
+         await ctx.send("Эти деревья являются абсолютными рекордсменами по выработки кислорода и устойчивочти к выхлопным газам, ежегодно одно дерево поглощает около 180 кг углекислого газа, а выделяет 30 кг кислорода.")
+         with open('images/дуб.jpg', 'rb') as f:
+             picture = discord.File(f)
+         await ctx.send(file=picture)
+         with open('images/каштан.jpg', 'rb') as f:
+             picture = discord.File(f)
+         await ctx.send(file=picture)
+         with open('images/тополь.jpg', 'rb') as f:
+             picture = discord.File(f)
+         await ctx.send(file=picture)
+         await ctx.send("Рекомендации по уходу: Эти деревья не прихотливы, поэтому уход за ними сводится к поливу и рыхлению почвы, подкормке удобрениями")
+
+    if indx == 3:
          await ctx.send("Чернозём")
+         await ctx.send("Шиповник, Сирень и Берёза предпочитают плодородную почву, поэтому чернозём для них лучший выбор.")
+         await ctx.send("Маленькие кустарники оказываются очень полезны для борьбы с пылевым загрязнением воздуха, они удерживают в 6 разбольше пыли, чем тополь")
+         with open('images/шиповник.jpg', 'rb') as f:
+             picture = discord.File(f)
+         await ctx.send(file=picture)
+         with open('images/сирень.jpg', 'rb') as f:
+             picture = discord.File(f)
+         await ctx.send(file=picture)
+         with open('images/береза.jpg', 'rb') as f:
+             picture = discord.File(f)
+         await ctx.send(file=picture)
+         await ctx.send("Рекомендации по уходу: данные растения любят хорошо удобренную и в меру влажную почву, но часто поддвержены вредителям, поэтому за ними требуются тщательный уход.")
+
          
 
 
